83d25af0277ca788111606e8636533610bebca3b
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-07-13 11:26:33 -0400

    iq moment

--- src/client/Latite.cpp
+++ src/client/Latite.cpp
@@ -11,6 +11,7 @@ alignas(Latite) char latiteBuf[sizeof(Latite)] = {};
 DWORD __stdcall startThread(HINSTANCE dll) {
     new (latiteBuf) Latite;
     Latite::get().initialize(dll);
+    return 0ul;
 }
 
 BOOL WINAPI DllMain(
@@ -19,12 +20,12 @@ BOOL WINAPI DllMain(
     LPVOID lpvReserved)  // reserved
 {
     if (fdwReason == DLL_PROCESS_ATTACH) {
-        CloseHandle(CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(startThread), hinstDLL, 0, 0));
+        CloseHandle(CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(startThread), hinstDLL, 0, nullptr));
     }
     return TRUE;  // Successful DLL_PROCESS_ATTACH.
 }
 
-Latite& Latite::get()
+Latite& Latite::get() noexcept
 {
     return *std::launder(reinterpret_cast<Latite*>(latiteBuf));
 }
