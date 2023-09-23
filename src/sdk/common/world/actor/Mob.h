2226530ec82d02eb3bac147a48e103d22797e989
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-09-23 16:26:33 -0400

    oops

--- src/sdk/common/world/actor/Mob.h
+++ src/sdk/common/world/actor/Mob.h
@@ -6,11 +6,11 @@ namespace SDK {
 	class Mob : public Actor {
 	public:
 		void setSprinting(bool b) {
-			memory::callVirtual<void>(this, MV_DETAIL_GETOFFSET(0x100, 0x115, 0x11C), b);
+			memory::callVirtual<void>(this, MV_DETAIL_GETOFFSET(0xFB, 0x115, 0x11C), b);
 		}
 
 		int getItemUseDuration() {
-			return memory::callVirtual<int>(this, MV_DETAIL_GETOFFSET(0x10F, 0x12A, 0x12F));
+			return memory::callVirtual<int>(this, MV_DETAIL_GETOFFSET(0x109, 0x12A, 0x12F));
 		}
 	};
 }
