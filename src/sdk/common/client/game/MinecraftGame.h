6c80882f0d00f640e45c60617ef766fa1563d68f
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-10-21 09:34:29 -0400

    oops

--- src/sdk/common/client/game/MinecraftGame.h
+++ src/sdk/common/client/game/MinecraftGame.h
@@ -10,9 +10,9 @@ namespace SDK {
 
 		MVCLASS_FIELD(std::string, xuid, 0x2F8, 0x618, 0x228);
 	private:
-		MVCLASS_FIELD(class FontRepository**, fontRepo, 0xF30, 0x110, 0xF88);
+		MVCLASS_FIELD(class FontRepository**, fontRepo, 0xF20, 0x110, 0xF88);
 	public:
-		MVCLASS_FIELD(class Font*, minecraftFont, 0xF40, 0x120, 0xF78);
+		MVCLASS_FIELD(class Font*, minecraftFont, 0xF30, 0x120, 0xF78);
 
 		class FontRepository* getFontRepository() {
 			if (internalVers > V1_19_41) {
