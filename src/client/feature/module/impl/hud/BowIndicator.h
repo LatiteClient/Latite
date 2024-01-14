0cbe7df8eee6a41bf2324bbff90be169d051f4b3
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-01-14 14:08:28 -0500

    Bow indicator default width matches v1

--- src/client/feature/module/impl/hud/BowIndicator.h
+++ src/client/feature/module/impl/hud/BowIndicator.h
@@ -10,7 +10,7 @@ private:
 	ValueType visual = BoolValue(false);
 	ValueType horizontal = BoolValue(false);
 	ValueType indicatorSize = FloatValue(120.f);
-	ValueType indicatorWidth = FloatValue(40.f);
+	ValueType indicatorWidth = FloatValue(30.f);
 	ValueType indicatorCol = ColorValue({ 0.1f, 0.1f, 0.1f, 0.8f });
 	ValueType indicatorCol2 = ColorValue({ 0.1f, 0.6f, 0.1f, 1.f });
 	ValueType indicatorRad = FloatValue(5.f);
