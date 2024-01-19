f08709bd783acd2dd37626df12303dc682972842
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-01-19 14:33:02 -0500

    fix 1.20.30-1.20.40 related ui bugs

--- src/sdk/common/client/game/MinecraftGame.cpp
+++ src/sdk/common/client/game/MinecraftGame.cpp
@@ -3,7 +3,7 @@
 #include "sdk/signature/storage.h"
 
 bool SDK::MinecraftGame::isCursorGrabbed() {
-    if (SDK::internalVers == VLATEST)
+    if (SDK::internalVers >= V1_20_30)
         return util::directAccess<bool>(this, Signatures::Offset::MinecraftGame_cursorGrabbed.result);
     // v1_18_12
     if (SDK::internalVers == V1_19_51) return util::directAccess<bool>(this, 0xD8);
