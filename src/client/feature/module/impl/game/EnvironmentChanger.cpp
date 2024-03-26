48d0336f72c65cff0a1d0b690344a925a1eeb6d1
Plextora <71889427+Plextora@users.noreply.github.com>
2024-03-26 18:59:34 -0400

    More precision in Time Changer

--- src/client/feature/module/impl/game/EnvironmentChanger.cpp
+++ src/client/feature/module/impl/game/EnvironmentChanger.cpp
@@ -2,15 +2,17 @@
 #include "EnvironmentChanger.h"
 #include <client/event/impl/WeatherEvent.h>
 
-EnvironmentChanger::EnvironmentChanger() : Module("EnvironmentChanger", "Environment Changer", "Changes visual features in the environment.", GAME) {
-	listen<WeatherEvent>((EventListenerFunc)&EnvironmentChanger::onWeather);
-	listen<FogColorEvent>((EventListenerFunc)&EnvironmentChanger::onFogColor);
-	listen<GetTimeEvent>((EventListenerFunc)&EnvironmentChanger::onTime);
+EnvironmentChanger::EnvironmentChanger() : Module("EnvironmentChanger", "Environment Changer",
+                                                  "Changes visual features in the environment.", GAME) {
+	listen<WeatherEvent>(static_cast<EventListenerFunc>(&EnvironmentChanger::onWeather));
+	listen<FogColorEvent>(static_cast<EventListenerFunc>(&EnvironmentChanger::onFogColor));
+	listen<GetTimeEvent>(static_cast<EventListenerFunc>(&EnvironmentChanger::onTime));
 
 	addSetting("setFogColor", "Set Fog Color", "", setFogColor);
 	addSetting("fogColor", "Fog Color", "The new fog color", fogColor, "setFogColor"_istrue);
 	addSetting("setTime", "Set Time", "Whether or not to set the time", setTime);
-	addSliderSetting("timeToSet", "Time", "The new time of day to set", time, FloatValue(0.f), FloatValue(1.f), FloatValue(0.1f), "setTime");
+	addSliderSetting("timeToSet", "Time", "The new time of day to set", time, FloatValue(0.f), FloatValue(1.f),
+	                 FloatValue(0.01f), "setTime");
 	addSetting("showWeather", "Show Weather", "Whether or not to show the weather", showWeather);
 }
 
