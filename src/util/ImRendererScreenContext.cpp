d4d7ee0fa3a7d974deb6913828435d3cd029e1a4
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-04-22 20:09:53 -0400

    Revert "+ImGui"
    This reverts commit 569e7e25d523b23810ac7c65a7532c57dd810ec2.

--- src/util/ImRendererScreenContext.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "ImRendererScreenContext.h"
-#include <sdk/common/client/renderer/Tessellator.h>
-
-void ImRendererScreenContext::renderDrawData(SDK::ScreenContext* screenContext, ImDrawData* data) {
-}
--- src/util/ImRendererScreenContext.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <imgui/imgui.h>
-
-namespace ImRendererScreenContext {
-	extern void renderDrawData(SDK::ScreenContext* screenContext, ImDrawData* data);
-}
