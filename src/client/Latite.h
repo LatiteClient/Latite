c3a4db8912558dfb0568812552d4aa9641a8f52f
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-05-05 17:18:16 -0400

    here we go.

--- src/client/Latite.h
+++ src/client/Latite.h
@@ -49,7 +49,7 @@ public:
 	Latite() = default;
 	~Latite() = default;
 
-	static constexpr std::string_view version = "v2.0.0b19";
+	static constexpr std::string_view version = "v2.0.0";
 	HINSTANCE dllInst = NULL;
 	std::string gameVersion;
 
