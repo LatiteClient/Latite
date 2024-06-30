b907d33c0a36e1c04cd948f5816e772a42d3c740
Plextora <71889427+Plextora@users.noreply.github.com>
2024-06-30 15:45:06 -0400

    Movable coordinates + option to hide coordinates

--- src/client/feature/module/impl/hud/Coordinates.cpp
+++ src/client/feature/module/impl/hud/Coordinates.cpp
@@ -3,15 +3,26 @@
 #include "Coordinates.h"
 #include <util/DxContext.h>
 
+#include "sdk/common/client/gui/controls/VisualTree.h"
+
 Coordinates::Coordinates() : HUDModule("Coordinates", "Coordinates", "Shows player position and other info", HUD) {
 	addSetting("showDimension", "Show Dimension", "Show the dimension the player is currently in", this->showDimension);
+	addSetting("hideVanillaCoordinates", "Hide vanilla coordinates", "", this->hideVanillaCoordinates);
+	addSetting(
+		"movableVanillaCoordinates",
+		"Movable vanilla coordinates",
+		"Instead of using Latite's coordinates, have vanilla coordinates be movable",
+		this->movableVanillaCoordinates);
+	listen<RenderLayerEvent>(static_cast<EventListenerFunc>(&Coordinates::onRenderLayer), true,
+	                         10 /*need to overpower the hud renderer*/);
 }
 
 void Coordinates::render(DrawUtil& dc, bool isDefault, bool inEditor) {
 	// todo: fix module looking weird in mod menu dropdown (rect issue)
-	// todo: option to hide vanilla coordinate display
 
 	if (!SDK::ClientInstance::get()->getLocalPlayer()) return;
+	if (std::get<BoolValue>(this->movableVanillaCoordinates)) return;
+
 	SDK::LocalPlayer* localPlayer = SDK::ClientInstance::get()->getLocalPlayer();
 
 	int playerPosX = static_cast<int>(localPlayer->getPos().x);
@@ -56,8 +67,48 @@ void Coordinates::render(DrawUtil& dc, bool isDefault, bool inEditor) {
 		rect.bottom = rect.top + moduleRc.bottom + dimensionRc.bottom;
 
 		dc.drawText(dimensionRc, dimensionModuleText, dimensionTextColor, Renderer::FontSelection::PrimaryRegular,
-			28.f, DWRITE_TEXT_ALIGNMENT_LEADING, DWRITE_PARAGRAPH_ALIGNMENT_NEAR, false);
+		            28.f, DWRITE_TEXT_ALIGNMENT_LEADING, DWRITE_PARAGRAPH_ALIGNMENT_NEAR, false);
 	}
 
 	dc.flush(true, false);
+}
+
+void Coordinates::onRenderLayer(Event& evG) {
+	RenderLayerEvent& ev = reinterpret_cast<RenderLayerEvent&>(evG);
+	SDK::LocalPlayer* localPlayer = SDK::ClientInstance::get()->getLocalPlayer();
+
+	if (!localPlayer) {
+		this->vanillaCoordinates = nullptr;
+		return;
+	}
+
+	if (this->isActive() && this->isEnabled()) {
+		if (ev.getScreenView()->visualTree->rootControl->name == XOR_STRING("hud_screen")) {
+			this->vanillaCoordinates = ev.getScreenView()->visualTree->rootControl->findFirstDescendantWithName(
+				XOR_STRING("player_position_text"));
+			float guiScale = SDK::ClientInstance::get()->getGuiData()->guiScale;
+
+			if (this->vanillaCoordinates && std::get<BoolValue>(this->hideVanillaCoordinates)) {
+				vanillaCoordinates->position = { 9999.f, 9999.f }; // very scuffed
+				updatePos();
+			}
+			else if (this->vanillaCoordinates && std::get<BoolValue>(this->movableVanillaCoordinates)) {
+				vanillaCoordinates->position = { rect.left / guiScale, rect.top / guiScale };
+				updatePos();
+
+				this->rect.right = rect.left + vanillaCoordinates->bounds.x * guiScale;
+				this->rect.bottom = rect.top + vanillaCoordinates->bounds.y * guiScale;
+			}
+			else {
+				vanillaCoordinates->position = { 3.f, 52.f }; // default vanilla coordinate position
+				updatePos();
+			}
+		}
+	}
+}
+
+void Coordinates::updatePos() {
+	vanillaCoordinates->getDescendants([](std::shared_ptr<SDK::UIControl> control) {
+		control->updatePos();
+	});
 }
--- src/client/feature/module/impl/hud/Coordinates.h
+++ src/client/feature/module/impl/hud/Coordinates.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "../../HUDModule.h"
+#include <sdk/common/client/gui/controls/UIControl.h>
 
 class Coordinates : public HUDModule {
 public:
 	Coordinates();
 
 	void render(DrawUtil& dc, bool, bool) override;
+	void onRenderLayer(Event& ev);
+	void updatePos();
 private:
+	SDK::UIControl* vanillaCoordinates;
 	ValueType showDimension = BoolValue(true);
+	ValueType hideVanillaCoordinates = BoolValue(false);
+	ValueType movableVanillaCoordinates = BoolValue(false);
 };
