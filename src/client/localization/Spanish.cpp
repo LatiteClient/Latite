c2bb46219135b02f4b136d38f72328a687bf6b02
Plextora <71889427+Plextora@users.noreply.github.com>
2024-07-22 15:44:35 -0400

    giving the spanish translation some love

--- src/client/localization/Spanish.cpp
+++ src/client/localization/Spanish.cpp
@@ -1,10 +1,16 @@
 #include "pch.h"
 #include "Spanish.h"
 
+// {} is a placeholder for a variable, similar to std::format (but I had to implement it myself).
+// When using {}, the LocalizeString::get call must be inside a util::FormatWString call to actually format it.
+
+// & is a placeholder for the format character Minecraft uses
+// When using &, the LocalizeString::get call must be inside a util::WFormat call to actually format it.
+
 Spanish::Spanish() {
     Spanish::strings = {
-        { "intro.welcome", L"¡Bienvenidos a Latite Client!" },
-        { "intro.menubutton", L"Apretón " + util::StrToWStr(util::KeyToString(Latite::get().getMenuKey().value)) + L" en un mundo o servidor abrir el menú de mods" }
+        { "client.intro.welcome", L"¡Bienvenidos a Latite Client!" },
+        { "client.intro.menubutton", L"Apretón {} en un mundo o servidor abrir el menú de mods" }
     };
 }
 
