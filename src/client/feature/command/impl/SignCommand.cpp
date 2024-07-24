e9ede30f68f200eafda43460902aeec9ce56e116
Plextora <71889427+Plextora@users.noreply.github.com>
2024-07-24 15:33:01 -0400

    🤫🧏🏻‍♂️

--- src/client/feature/command/impl/SignCommand.cpp
+++ src/client/feature/command/impl/SignCommand.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "SignCommand.h"
 
-SignCommand::SignCommand() : Command("sign", "Signs a script", "<script>", {"cert"}) {
+SignCommand::SignCommand() : Command("sign", L"Signs a script", "<script>", {"cert"}) {
 }
 
 bool SignCommand::execute(std::string const label, std::vector<std::string> args) {
@@ -10,20 +10,21 @@ bool SignCommand::execute(std::string const label, std::vector<std::string> args
 	if (args.empty()) return false;
 	auto path = util::GetLatitePath() / "Plugins" / args[0];
 	if (!std::filesystem::exists(path)) {
-		message("Could not find script " + path.string(), true);
+		// todo: check if converting a generic_string to std::wstring actually works
+		message(L"Could not find script " + util::StrToWStr(path.generic_string()), true);
 		return true;
 	}
 	auto cert = JsPlugin::getHash(path);
 	if (cert) {
 #if LATITE_DEBUG
-		message(std::format("Generated certificate {}", util::WStrToStr(cert.value())));
+		message(util::StrToWStr(std::format("Generated certificate {}", util::WStrToStr(cert.value()))));
 #endif
 		std::wofstream ofs(path / XOR_STRING("certificate"));
 		ofs << cert.value();
 		ofs.flush();
 		return true;
 	}
-	message(std::format("Could not create certificate for script {}", path.string(), true));
+	message(util::StrToWStr(std::format("Could not create certificate for script {}", path.string(), true)));
 #endif
 	return true;
 }
