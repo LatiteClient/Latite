e0a7e4542f494368133dddd3bdc92ed89476f6ea
FreezeEngine <mr.springtrap@yandex.ru>
2024-08-23 04:29:37 +0300

    Cleanup

--- src/client/feature/module/impl/visual/NoHurtCam.h
+++ src/client/feature/module/impl/visual/NoHurtCam.h
@@ -1,11 +1,9 @@
 #pragma once
 #include "../../Module.h"
-#include "client/event/impl/GammaEvent.h"
 
 class NoHurtCam : public Module {
 public:
 	NoHurtCam();
 
 	void onBobHurt(Event& ev);
-private:
 };
