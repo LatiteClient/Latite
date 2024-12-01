1648283c5d952847f94659123aa3535fd74d9442
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-12-01 12:47:00 -0500

    [Scripting] Screen setKey, getKey, isHover

--- src/client/script/class/impl/JsScreenClass.h
+++ src/client/script/class/impl/JsScreenClass.h
@@ -8,10 +8,12 @@ protected:
 	static JsValueRef CALLBACK jsConstructor(JsValueRef callee, bool isConstructor,
 		JsValueRef* arguments, unsigned short argCount, void* callbackState) {
 		auto thi = reinterpret_cast<JsScreenClass*>(callbackState);
-		if (!Chakra::VerifyArgCount(argCount, 1)) return JS_INVALID_REFERENCE;
-		if (!Chakra::VerifyParameters({ {arguments[1], JsString} })) return JS_INVALID_REFERENCE;
+		if (!Chakra::VerifyArgCount(argCount, 2)) return JS_INVALID_REFERENCE;
+		if (!Chakra::VerifyParameters({ {arguments[1], JsString}, {arguments[2], JsNumber} })) return JS_INVALID_REFERENCE;
 
 		auto screen = new JsScreen(arguments[0], util::WStrToStr(Chakra::GetString(arguments[1])));
+		screen->key = Chakra::GetInt(arguments[2]);
+
 		JsValueRef obj = thi->construct(screen, false);
 		return obj;
 	}
@@ -28,6 +30,40 @@ protected:
 		JsValueRef* arguments, unsigned short argCount, void* callbackState) {
 
 		return Get(arguments[0])->isActive() ? Chakra::GetTrue() : Chakra::GetFalse();
+	}
+
+	static JsValueRef CALLBACK getKeyCallback(JsValueRef callee, bool isConstructor,
+		JsValueRef* arguments, unsigned short argCount, void* callbackState) {
+
+		return Chakra::MakeInt(Get(arguments[0])->key);
+	}
+
+	static JsValueRef CALLBACK setKeyCallback(JsValueRef callee, bool isConstructor,
+		JsValueRef* arguments, unsigned short argCount, void* callbackState) {
+		if (!Chakra::VerifyArgCount(argCount, 2)) return JS_INVALID_REFERENCE;
+		if (!Chakra::VerifyParameters({ {arguments[1], JsNumber} })) return JS_INVALID_REFERENCE;
+
+		Get(arguments[0])->key = Chakra::GetInt(arguments[1]);
+		return JS_INVALID_REFERENCE;
+	}
+
+	static JsValueRef CALLBACK isHoverCallback(JsValueRef callee, bool isConstructor,
+		JsValueRef* arguments, unsigned short argCount, void* callbackState) {
+
+		if (!Chakra::VerifyArgCount(argCount, 2, true, true)) return JS_INVALID_REFERENCE;
+		if (!Chakra::VerifyParameters({ {arguments[1], JsObject}, {Chakra::TryGet(arguments, argCount, 2), JsObject}})) return JS_INVALID_REFERENCE;
+
+		auto obj = reinterpret_cast<JsScreenClass*>(callbackState);
+
+		auto rect = JsRect::ToRect(arguments[1]);
+		
+		
+		Vec2 point = SDK::ClientInstance::get()->cursorPos;
+		if (auto val = Chakra::TryGet(arguments, argCount, 2)) {
+			point = JsVec2::ToVec2(val);
+		}
+
+		return Get(arguments[0])->shouldSelect(rect, point) ? Chakra::GetTrue() : Chakra::GetFalse();
 	}
 public:
 	inline static const wchar_t* class_name = L"Screen";
@@ -52,5 +88,8 @@ public:
 	void prepareFunctions() override {
 		Chakra::SetPropertyString(prototype, L"name", L"", true);
 		Chakra::DefineFunc(prototype, isActiveCallback, L"isActive", this);
+		Chakra::DefineFunc(prototype, getKeyCallback, L"getKey", this);
+		Chakra::DefineFunc(prototype, setKeyCallback, L"setKey", this);
+		Chakra::DefineFunc(prototype, isHoverCallback, L"isHover", this);
 	};
 };
