d2bcf1787786d23725e5b6d8882829227cfcc987
Plextora <71889427+Plextora@users.noreply.github.com>
2024-12-30 15:50:19 -0500

    Update Latite::version to v2.3.4

--- src/client/Latite.h
+++ src/client/Latite.h
@@ -53,7 +53,7 @@ public:
 	Latite() = default;
 	~Latite() = default;
 
-	static constexpr std::string_view version = "v2.3.3";
+	static constexpr std::string_view version = "v2.3.4";
 	HINSTANCE dllInst = NULL;
 	std::string gameVersion;
 
