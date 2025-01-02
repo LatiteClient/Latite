5272fa9d089e3dcfc2b0a5cb021956c242d2d00d
Imrglop <69129770+Imrglop@users.noreply.github.com>
2025-01-01 23:12:26 -0500

    tweak isTrusted and checkTrusted

--- src/client/script/JsPlugin.cpp
+++ src/client/script/JsPlugin.cpp
@@ -21,7 +21,33 @@ using namespace winrt::Windows::Web::Http;
 using namespace winrt::Windows::Web::Http::Filters;
 
 
-void JsPlugin::checkTrusted() {}
+void JsPlugin::checkTrusted() {
+	trusted = false;
+	std::string url = "https://raw.githubusercontent.com/Imrglop/Latite-Releases/refs/heads/main/script_hashes.txt";
+	HttpClient client;
+	winrt::Windows::Foundation::Uri uri(winrt::to_hstring(url));
+
+	HttpRequestMessage request(HttpMethod::Get(), uri);
+	request.Headers().Insert(winrt::to_hstring("User-Agent"), winrt::to_hstring("WinRTApp"));
+
+	HttpResponseMessage response = client.SendRequestAsync(request).get();
+	if (response.StatusCode() != HttpStatusCode::Ok) {
+		Logger::Info("Plugin trust check request failed: {}", static_cast<int>(response.StatusCode()));
+		return;
+	}
+
+	std::string stringResponse = to_string(response.Content().ReadAsStringAsync().get());
+	std::optional<std::wstring> currentHash = getHash(this->getPath());
+
+	std::istringstream stream(stringResponse);
+	std::string hash;
+	while (std::getline(stream, hash)) {
+		if (currentHash == util::StrToWStr(hash)) {
+			trusted = true;
+			return;
+		}
+	}
+}
 
 JsPlugin::JsPlugin(std::wstring const& relPath) {
 	this->path = this->path / relPath;
@@ -49,6 +75,9 @@ bool JsPlugin::load() {
 		JS::JsDisposeRuntime(this->runtime);
 		this->runtime = JS_INVALID_RUNTIME_HANDLE;
 	}
+
+	// Check plugin permissions
+	checkTrusted();
 	return mainScript != nullptr;
 }
 
@@ -235,29 +264,5 @@ std::optional<std::wstring> JsPlugin::getHash(std::filesystem::path const& main)
 }
 
 bool JsPlugin::isTrusted() {
-    std::string url = "https://raw.githubusercontent.com/Imrglop/Latite-Releases/refs/heads/main/script_hashes.txt";
-    HttpClient client;
-    winrt::Windows::Foundation::Uri uri(winrt::to_hstring(url));
-
-    HttpRequestMessage request(HttpMethod::Get(), uri);
-    request.Headers().Insert(winrt::to_hstring("User-Agent"), winrt::to_hstring("WinRTApp"));
-
-    HttpResponseMessage response = client.SendRequestAsync(request).get();
-    if (response.StatusCode() != HttpStatusCode::Ok) {
-		Logger::Info("Plugin trust check request failed: {}", static_cast<int>(response.StatusCode()));
-        return false;
-    }
-
-    std::string stringResponse = to_string(response.Content().ReadAsStringAsync().get());
-    std::optional<std::wstring> currentHash = getHash(this->getPath());
-
-    std::istringstream stream(stringResponse);
-    std::string hash;
-    while (std::getline(stream, hash)) {
-        if (currentHash == util::StrToWStr(hash)) {
-            return true;
-        }
-    }
-
-    return false;
+	return trusted;
 }
