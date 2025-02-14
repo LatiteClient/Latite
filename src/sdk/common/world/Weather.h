18456ad4579d625c8678ac8a8ce7b9d2bd15d41b
marioCST <68396929+marioCST@users.noreply.github.com>
2025-02-14 18:50:29 +0100

    Fix EnvironmentChanger show weather

--- src/sdk/common/world/Weather.h
+++ src/sdk/common/world/Weather.h
@@ -6,13 +6,13 @@ namespace SDK {
 		char pad_0008[40]; //0x0008
 	public:
 		struct WeatherData {
-			float rainTransitionOld; //0x0034
-			float rainTransition; //0x0038
-			float rain; //0x003C
-			float thunderTransitionOld; //0x0040
-			float thunderTransition; //0x0044
-			float thunder; //0x0048
-			float skyDarkness; //0x004C
+			float rainTransitionOld{}; //0x0034
+			float rainTransition{}; //0x0038
+			float rain{}; //0x003C
+			float thunderTransitionOld{}; //0x0040
+			float thunderTransition{}; //0x0044
+			float thunder{}; //0x0048
+			float skyDarkness{}; //0x004C
 
 			WeatherData() = default;
 		};
