f9715a471ed4787625032e31fff1f54a363d028a
Plextora <71889427+Plextora@users.noreply.github.com>
2025-06-11 14:15:50 -0400

    Added a visual bar to BreakIndicator (finally)

--- src/client/feature/module/impl/hud/BreakIndicator.cpp
+++ src/client/feature/module/impl/hud/BreakIndicator.cpp
@@ -7,9 +7,77 @@ BreakIndicator::BreakIndicator() : TextModule("BreakProgress",
                                               0, true) {
     addSetting("last", LocalizeString::get("client.textmodule.breakIndicator.lastProgress.name"),
                LocalizeString::get("client.textmodule.breakIndicator.lastProgress.desc"), this->last);
+    addSetting("visual", LocalizeString::get("client.textmodule.breakIndicator.visual.name"),
+               LocalizeString::get("client.textmodule.breakIndicator.visual.desc"), this->visual);
+    addSetting("fgCol", LocalizeString::get("client.textmodule.breakIndicator.fgCol.name"),
+               LocalizeString::get("client.textmodule.breakIndicator.fgCol.desc"), this->indicatorCol2,
+               "visual"_istrue);
+    addSetting("bgCol", LocalizeString::get("client.textmodule.breakIndicator.bgCol.name"),
+               LocalizeString::get("client.textmodule.breakIndicator.bgCol.desc"), this->indicatorCol, "visual"_istrue);
+    addSetting("horizontal", LocalizeString::get("client.textmodule.breakIndicator.horizontal.name"),
+               LocalizeString::get("client.textmodule.breakIndicator.horizontal.desc"), this->horizontal,
+               "visual"_istrue);
+
+    addSliderSetting("size", LocalizeString::get("client.textmodule.breakIndicator.size.name"),
+                     LocalizeString::get("client.textmodule.breakIndicator.size.desc"), indicatorSize, FloatValue(0.f),
+                     FloatValue(200.f), FloatValue(2.5f), "visual"_istrue);
+    addSliderSetting("width", LocalizeString::get("client.textmodule.breakIndicator.width.name"),
+                     LocalizeString::get("client.textmodule.breakIndicator.width.desc"), indicatorWidth,
+                     FloatValue(0.f),
+                     FloatValue(200.f), FloatValue(2.5f), "visual"_istrue);
+    addSliderSetting("rad", LocalizeString::get("client.textmodule.breakIndicator.rad.name"),
+                     LocalizeString::get("client.textmodule.breakIndicator.rad.desc"), indicatorRad, FloatValue(0.f),
+                     FloatValue(5.f), FloatValue(1.f), "visual"_istrue);
+    addSliderSetting("padding", LocalizeString::get("client.textmodule.breakIndicator.padding.name"),
+                     LocalizeString::get("client.textmodule.breakIndicator.padding.desc"), padding, FloatValue(0.f),
+                     FloatValue(20.f), FloatValue(1.f), "visual"_istrue);
 }
 
 BreakIndicator::~BreakIndicator() {
+}
+
+void BreakIndicator::render(DrawUtil& dc, bool isDefault, bool inEditor) {
+    if (!std::get<BoolValue>(visual)) {
+        TextModule::render(dc, isDefault, inEditor);
+        return;
+    }
+
+    auto lp = SDK::ClientInstance::get()->getLocalPlayer();
+    if (!lp) return;
+
+    float prog = std::get<BoolValue>(last) ? lp->gameMode->lastBreakProgress : lp->gameMode->breakProgress;
+
+    if (prog == 0.f) {
+        return;
+    }
+
+    bool horiz = std::get<BoolValue>(horizontal);
+    float wid = std::get<FloatValue>(indicatorWidth);
+    float siz = std::get<FloatValue>(indicatorSize);
+
+    d2d::Rect rc = {0.f, 0.f, horiz ? siz : wid, horiz ? wid : siz};
+    float rad = std::get<FloatValue>(indicatorRad) / 10.f * (std::min)(rc.getWidth(), rc.getHeight());
+
+    rect.right = rect.left + rc.getWidth();
+    rect.bottom = rect.top + rc.getHeight();
+
+    dc.fillRoundedRectangle(rc, std::get<ColorValue>(indicatorCol).getMainColor(), rad);
+
+    d2d::Rect fillRc = rc;
+
+    float pad = std::get<FloatValue>(padding);
+    fillRc.left += pad;
+    fillRc.top += pad;
+    fillRc.right -= pad;
+    fillRc.bottom -= pad;
+
+    if (horiz) {
+        fillRc.right = fillRc.left + fillRc.getWidth() * prog;
+    }
+    else {
+        fillRc.top = fillRc.bottom - fillRc.getHeight() * prog;
+    }
+    dc.fillRoundedRectangle(fillRc, std::get<ColorValue>(indicatorCol2).getMainColor(), rad);
 }
 
 std::wstringstream BreakIndicator::text(bool isDefault, bool inEditor) {
--- src/client/feature/module/impl/hud/BreakIndicator.h
+++ src/client/feature/module/impl/hud/BreakIndicator.h
@@ -3,11 +3,21 @@
 
 class BreakIndicator : public TextModule {
 private:
-	ValueType last = BoolValue(false);
+    ValueType last = BoolValue(false);
+    ValueType visual = BoolValue(false);
+    ValueType horizontal = BoolValue(false);
+    ValueType indicatorSize = FloatValue(120.f);
+    ValueType indicatorWidth = FloatValue(30.f);
+    ValueType indicatorCol = ColorValue({0.1f, 0.1f, 0.1f, 0.8f});
+    ValueType indicatorCol2 = ColorValue({0.1f, 0.6f, 0.1f, 1.f});
+    ValueType indicatorRad = FloatValue(0.f);
+    ValueType padding = FloatValue(6.f);
+
 public:
-	BreakIndicator();
-	virtual ~BreakIndicator();
+    BreakIndicator();
+    ~BreakIndicator() override;
 
 private:
-	std::wstringstream text(bool isDefault, bool inEditor) override;
+    void render(DrawUtil& dc, bool isDefault, bool inEditor) override;
+    std::wstringstream text(bool isDefault, bool inEditor) override;
 };
