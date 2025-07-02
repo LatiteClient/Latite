8bb2e3be8d41f149f42f58219eace967d9ddf046
Plextora <71889427+Plextora@users.noreply.github.com>
2025-07-01 22:46:45 -0400

    RUNNING IT BACK 🗣️🗣️🗣️

--- src/client/Latite.cpp
+++ src/client/Latite.cpp
@@ -511,6 +511,7 @@ void Latite::threadsafeInit() {
 
     auto app = winrt::Windows::UI::ViewManagement::ApplicationView::GetForCurrentView();
     std::string vstr(this->version);
+	
 #if defined(LATITE_NIGHTLY)
     auto ws = util::StrToWStr("Latite Client [NIGHTLY] " + gameVersion + " " + vstr + "/" + calcCurrentDLLHash());
 #elif defined(LATITE_DEBUG)
