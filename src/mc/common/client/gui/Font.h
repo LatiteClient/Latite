da1f283b79152d55d93cb94029a731962f1fe14e
phase <64127681+phasephasephase@users.noreply.github.com>
2026-05-06 17:02:23 -0600

    correct vtable indices for Font

--- src/mc/common/client/gui/Font.h
+++ src/mc/common/client/gui/Font.h
@@ -7,11 +7,11 @@ namespace SDK {
 	public:
 		int getLineLength(std::string_view str, float fontSize, bool showColorSymbol = false) {
 			using Fn = int(__thiscall*)(Font*, std::string_view, float, bool);
-			return reinterpret_cast<Fn>(vtable[0x6])(this, str, fontSize, showColorSymbol);
+			return reinterpret_cast<Fn>(vtable[0x30])(this, str, fontSize, showColorSymbol);
 		}
 
 		float getLineHeight() {
-			return reinterpret_cast<float(*)(Font*)>(vtable[0x7])(this);
+			return reinterpret_cast<float(*)(Font*)>(vtable[0x38])(this);
 		}
 	};
 }
