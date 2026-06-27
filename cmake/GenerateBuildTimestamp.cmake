8aa96209d4d2397cb28f552023ae711a4898d1a3
Plextora <71889427+Plextora@users.noreply.github.com>
2026-06-27 17:01:30 -0400

    Create build timestamp in cmake instead of a powershell script

--- cmake/GenerateBuildTimestamp.cmake
+++ cmake/GenerateBuildTimestamp.cmake
@@ -2,7 +2,11 @@ if (NOT DEFINED OUTPUT_FILE OR OUTPUT_FILE STREQUAL "")
     message(FATAL_ERROR "OUTPUT_FILE is required")
 endif ()
 
-set(build_timestamp "0000-00-00_00-00-00Z")
+if (DEFINED LATITE_BUILD_TIMESTAMP AND NOT LATITE_BUILD_TIMESTAMP STREQUAL "")
+    set(build_timestamp "${LATITE_BUILD_TIMESTAMP}")
+else ()
+    string(TIMESTAMP build_timestamp "%Y-%m-%d_%H-%M-%SZ" UTC)
+endif ()
 
 if (NOT build_timestamp MATCHES
         "^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9]Z$")
@@ -16,5 +20,9 @@ file(WRITE "${OUTPUT_FILE}"
 "#include \"client/BuildTimestamp.h\"\n\
 \n\
 const char* LatiteBuild::getTimestamp() noexcept {\n\
+#if defined(LATITE_DEBUG) || defined(LATITE_NIGHTLY)\n\
     return \"${build_timestamp}\";\n\
+#else\n\
+    return \"0000-00-00_00-00-00Z\";\n\
+#endif\n\
 }\n")
