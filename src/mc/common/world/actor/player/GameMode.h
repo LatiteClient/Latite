351c08517e0e8601d801f4e8db3de56964c5ca63
Yuki <coolyu123@126.com>
2026-09-12 10:59:49 +0800

    feat: update GameMode sdk

--- src/mc/common/world/actor/player/GameMode.h
+++ src/mc/common/world/actor/player/GameMode.h
@@ -1,36 +1,106 @@
 #pragma once
+
 #include "util/LMath.h"
 
 namespace SDK {
-    class GameMode {
-    public:
-        class Player* plr;
 
-    private:
-        [[maybe_unused]] char pad[16];
+class GameMode {
+public:
+    class Player* plr;
 
-    public:
-        float lastBreakProgress;
-        float breakProgress;
+private:
+    [[maybe_unused]] char pad[16];
 
-        virtual ~GameMode() = 0;
-        virtual bool startDestroyBlock(BlockPos const&, uint8_t, bool&) = 0;
-        virtual bool destroyBlock(BlockPos const&, uint8_t) = 0;
-        virtual bool continueDestroyBlock(BlockPos const&, uint8_t, bool&) = 0;
-        virtual void stopDestroyBlock(BlockPos const&) = 0;
-        virtual void startBuildBlock(BlockPos const&, uint8_t) = 0;
-        virtual bool buildBlock(BlockPos const&, uint8_t) = 0;
-        virtual void continueBuildBlock(BlockPos const&, uint8_t) = 0;
-        virtual void stopBuildBlock(void) = 0;
-        virtual void tick(void) = 0;
-        virtual void getPickRange(void*, bool) = 0;
-        virtual void useItem(class ItemStack*) = 0;
-        virtual void useItemOn(ItemStack*, BlockPos const&, unsigned char, Vec3 const&, class Block const*) = 0;
-        virtual void interact(class Actor*, Vec3 const&) = 0;
-        virtual void attack(Actor*) = 0;
-        virtual void releaseUsingItem(void) = 0;
-        virtual void setTrialMode(bool) = 0;
-        virtual bool isInTrialMode(void) = 0;
-        virtual void registerUpsellScreenCallback(void*) = 0;
-    };
+public:
+    float lastBreakProgress;
+    float breakProgress;
+
+    virtual ~GameMode() = 0;
+
+    virtual bool startDestroyBlock(
+        BlockPos const&,
+        uint8_t,
+        bool&
+    ) = 0;
+
+    virtual bool destroyBlock(
+        BlockPos const&,
+        uint8_t
+    ) = 0;
+
+    virtual bool continueDestroyBlock(
+        BlockPos const&,
+        uint8_t,
+        Vec3 const&,
+        bool&
+    ) = 0;
+
+    virtual void stopDestroyBlock(
+        BlockPos const&
+    ) = 0;
+
+    virtual void startBuildBlock(
+        BlockPos const&,
+        uint8_t
+    ) = 0;
+
+    virtual bool buildBlock(
+        BlockPos const&,
+        uint8_t,
+        bool
+    ) = 0;
+
+    virtual void continueBuildBlock(
+        BlockPos const&,
+        uint8_t
+    ) = 0;
+
+    virtual void stopBuildBlock() = 0;
+
+    virtual void tick() = 0;
+
+    virtual float getPickRange(
+        /* InputMode const& */
+        void*
+    ) = 0;
+
+    virtual bool useItem(
+        ItemStack*
+    ) = 0;
+
+    virtual bool useItemAsAttack(
+        ItemStack*,
+        Vec3 const&
+    ) = 0;
+
+    virtual InteractionResult useItemOn(
+        ItemStack*,
+        BlockPos const&,
+        uint8_t,
+        Vec3 const&,
+        Block const*,
+        bool
+    ) = 0;
+
+    virtual bool interact(
+        Actor*,
+        Vec3 const&
+    ) = 0;
+
+    virtual bool attack(
+        Actor*,
+        Vec3 const&
+    ) = 0;
+
+    virtual void releaseUsingItem() = 0;
+
+    virtual void setTrialMode(bool) = 0;
+
+    virtual bool isInTrialMode() = 0;
+
+    virtual void registerUpsellScreenCallback(
+        void*
+    ) = 0;
+};
+
 }
